a57f35b76c75801d2c395fb61629e2c57d96f0ee
Loctarogar <igrsl1990@gmail.com>
2019-04-06 22:47:10 +0300

    readme + migrations

--- MIGRATIONS/db.php
+++ MIGRATIONS/db.php
@@ -1,3 +1,21 @@
+CREATE TABLE `first` (
+`RecordId` int(11) DEFAULT NULL,
+`Date` date DEFAULT NULL,
+`Time` time DEFAULT NULL,
+`EventId` int(11) DEFAULT NULL,
+`UserId` int(11) DEFAULT NULL,
+`ClientId` int(11) DEFAULT NULL
+)
+
+INSERT INTO first
+VALUES (1, '2019-02-01', '15:00:00', 2, 100, 5),
+       (2, '2019-02-01', '17:00:00', 2, 150, 5),
+       (3, '2019-02-05', '12:00:00', 2, 200, 5),
+       (4, '2019-02-05', '15:00:00', 3, 100, 5),
+       (5, '2019-02-06', '15:00:00', 2, 200, 5),
+       (6, '2019-02-10', '09:00:00', 4, 150, 5),
+       (7, '2019-02-10', '09:00:00', 4, 100, 5)
+
 CREATE TABLE buildings (
 BuildId INT,
 Address VARCHAR(50)
